5df36e77b7d431719593a05cb2b6d32cb21b1e0f
Guadalupe Isidro (LÜ ANGELES) <51386526+LuAngeles@users.noreply.github.com>
2019-07-25 14:12:03 -0500

    Agregando sección ahorro y menú de gastos

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ const sumar=()=>{
     total=total.toFixed(2);
     document.getElementById('gasto-total').innerHTML=total;
 
-
 }
 
+var slider=document.getElementById("slider");
+var val=document.getElementById("value");
+val.innerHTML=slider.value+"%";
+slider.oninput=function(){
+    val.innerHTML=this.value+"%";
+}
